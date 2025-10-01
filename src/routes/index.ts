355a0fe6707cfc722c0db56afc2a056bb5f7ee55
Jorge Matos <jisapjisap@gmail.com>
2025-10-01 13:09:46 +0200

    Add logo assets and implement routing structure
    - Added new logo files in ICO, PNG, and SVG formats.
    - Removed App.css to streamline styling approach.
    - Integrated React Router for navigation, replacing the App component with RouterProvider.
    - Created a basic Login page and defined initial routes in index.ts.

---
+++ src/routes/index.ts
@@ -0,0 +1,59 @@
+import { Login } from "@/pages/auth/Login";
+import { createBrowserRouter } from "react-router";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    Component: Login,
+  },
+  {
+    path: "/signup"
+    //Component: Signup,
+  },
+  {
+    path: "/refresh-token",
+  },
+  {
+    path: "/",
+    children: [
+      {
+        index: true,
+      },
+      {
+        path: "blogs"
+      },
+      {
+        path: "blogs/:slug",
+      }    
+    ]
+  },
+  {
+    path: "/admin",
+    children: [
+      {
+        path: "dashboard",
+      },
+      {
+        path: "blogs",
+      },
+      {
+        path: "blogs/create",
+      },
+      {
+        path: "blogs/:slug/edit",
+      },
+      {
+        path: "comments"
+      },
+      {
+        path: "users"
+      }
+    ]
+  },
+  {
+    path: "/settings",
+  }
+]);
+
+export default router;
