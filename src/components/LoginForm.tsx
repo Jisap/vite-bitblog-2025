70b412079035e01331ba9234794239111d6168ad
Jorge Matos <jisapjisap@gmail.com>
2025-10-02 18:34:32 +0200

    Add footer to LoginForm with links to Terms of Service and Privacy Policy

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -162,6 +162,10 @@ export const LoginForm = ({ className, ...props }: React.ComponentProps<'div'>) 
           </figure>
         </CardContent>
       </Card>
+
+      <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}and <a href="#">Privacy Policy</a>
+      </div>
     </div>
   )
 }
