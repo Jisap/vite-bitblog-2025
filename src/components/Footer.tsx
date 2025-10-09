31d50d2ec7dad5dd2c8409530402b06951943b70
Jorge Matos <jisapjisap@gmail.com>
2025-10-09 15:19:14 +0200

    Add Footer component and integrate Tooltip functionality
    - Introduced Footer component to display social media links with tooltips for better user interaction.
    - Added Tooltip component using Radix UI for enhanced UI feedback.
    - Updated RootLayout to include the new Footer component.
    - Updated package.json and package-lock.json to include new dependency: @radix-ui/react-tooltip.

---
+++ src/components/Footer.tsx
@@ -0,0 +1,71 @@
+import { cn } from "@/lib/utils"
+import { Logo } from "./Logo"
+import { Button } from "./ui/button"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Facebook, Instagram, Linkedin, Youtube } from "lucide-react";
+import { Separator } from '@/components/ui/separator';
+import { href } from "react-router";
+
+const SOCIAL_LINKS = [
+  {
+    href: "https://www.facebook.com/bitblog",
+    Icon: Facebook,
+    label: "Facebook",
+  },
+  {
+    href: "https://www.instagram.com/bitblog",
+    Icon: Instagram,
+    label: "Instagram",
+  },
+  {
+    href: "https://www.linkedin.com/company/bitblog",
+    Icon: Linkedin,
+    label: "Linkedin",
+  },
+  {
+    href: "https://www.youtube.com/channel/UC-8-y-7-9-5-6-2",
+    Icon: Youtube,
+    label: "Youtube",
+  },
+] as const;
+
+
+export const Footer = ({ className, ...props}: React.ComponentProps<"footer">) => {
+  return (
+    <footer className={cn("border-t", className)}>
+      <div className="container py-8 grid max-md:justify-items-center md:grid-cols-[1fr_3fr_1fr] md:items-center">
+        <Logo />
+
+        <p className="text-muted-foreground order-1 max-md:text-center md:order-none md:justify-self-center">
+          &copy; {new Date().getFullYear()} BitBlog. All rights reserved.
+        </p>
+
+        <ul className="flex items-center gap-1 max-md:mt-6 max-md:mb-4 md:justify-self-end">
+          {SOCIAL_LINKS.map(({ href, Icon, label }) => (
+            <li key={href}>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button 
+                    variant="ghost"
+                    size="icon"
+                    arial-label={label}
+                    asChild
+                  > 
+                    <a href={href} target="_blank">
+                      <Icon />
+                    </a>
+                  </Button>
+                </TooltipTrigger>
+              </Tooltip>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
+  )
+}
+
--- src/components/layouts/Roots.tsx
+++ src/components/layouts/Roots.tsx
@@ -3,17 +3,19 @@
 import { Outlet } from "react-router";
 import Loading from "../Loading";
 import { Header } from "../Header";
+import { Footer } from "../Footer";
 
 
 
 export const RootLayout = () => {
   return (
-    <div className="flex flex-col min-g-dvh">
+    <div className="flex flex-col min-h-dvh">
       <Loading className="z-40" />
       <Header />
       <main className="grow flex flex-col">
         <Outlet />
       </main>
+      <Footer />
     </div>
   )
 }
