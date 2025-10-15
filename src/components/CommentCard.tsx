c716fae9a725b328a5bf98123d168dd2adc00e61
Jorge Matos <jisapjisap@gmail.com>
2025-10-15 20:07:59 +0200

    Enhance CommentCard component with blog details and improved layout
    - Added conditional rendering for blog information, including a banner image and title, enhancing the visual presentation of comments.
    - Improved the structure of the CommentCard by organizing content into distinct sections for better readability and user interaction.
    - Included a link to the blog for easy navigation, further enriching the user experience.

--- src/components/CommentCard.tsx
+++ src/components/CommentCard.tsx
@@ -24,19 +24,22 @@ export const CommentCard = ({ content, likesCount, user, blog, createdAt }: Prop
   return (
     <div className="@container">
       <div className="group flex flex-col items-start gap-4 p-4 rounded-xl hover:bg-accent/25 @md:flex-row">
-        <Avatar 
-          email={user?.email} 
+        {/* Avatar */}
+        <Avatar
+          email={user?.email}
           size="40"
           round
         />
 
+        {/* Content */}
         <div className="flex flex-col gap-2 me-auto">
+          {/* Author */}
           <div className="flex items-center gap-2">
             {user ? (
               <div className="text-sm text-muted-foreground">
                 @{user.username}
               </div>
-            ):(
+            ) : (
               <div className="text-sm text-destructive/80 italic">
                 <Tooltip delayDuration={250}>
                   <TooltipTrigger>
@@ -51,11 +54,11 @@ export const CommentCard = ({ content, likesCount, user, blog, createdAt }: Prop
             )}
 
             <div className="size-1 rounded-full bg-muted-foreground/50"></div>
-          
+
             <div className="text-sm text-muted-foreground">
               <Tooltip delayDuration={250}>
                 <TooltipTrigger>
-                  {formatDistanceToNow(createdAt, {addSuffix: true})}
+                  {formatDistanceToNow(createdAt, { addSuffix: true })}
                 </TooltipTrigger>
 
                 <TooltipContent>
@@ -68,10 +71,12 @@ export const CommentCard = ({ content, likesCount, user, blog, createdAt }: Prop
             </div>
           </div>
 
+          {/* comment content */}
           <div className="max-w-[60ch]">
             {content}
           </div>
 
+          {/* Likes and actions */}
           <div className="flex items-center gap-2 mt-1">
             <Button variant="ghost" aria-label="Like comment">
               <ThumbsUpIcon />
@@ -87,6 +92,40 @@ export const CommentCard = ({ content, likesCount, user, blog, createdAt }: Prop
             </Button>
           </div>
         </div>
+
+        {blog && (
+          <>
+            <div className="max-w-80 grid grid-cols-[120px_minmax(200px,_1fr)] gap-3 @max-3xl:hidden">
+              <AspectRatio
+                ratio={21 / 9}
+                className="rounded-lg overflow-hidden"
+              >
+                <img
+                  src={blog.banner.url}
+                  alt={blog.title}
+                  width={blog.banner.width}
+                  height={blog.banner.height}
+                />
+              </AspectRatio>
+
+              <div className="line-clamp-3 max-w-[30ch] text-sm text-muted-foreground my-1">
+                {blog.title}
+              </div>
+            </div>
+
+            <Button 
+              variant="ghost"
+              className=""
+              asChild
+            >
+              <Link to={`/blogs/${blog.slug}`}>
+                <span className="@md:hidden">Go to blog</span>
+                <SquareArrowOutUpRightIcon  />
+              </Link>
+            </Button>
+          </>
+        )}
+
       </div>
     </div>
   )
