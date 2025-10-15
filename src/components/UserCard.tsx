0a9d3a5d1894772ca7083e02c42abf08b52601ca
Jorge Matos <jisapjisap@gmail.com>
2025-10-15 20:57:22 +0200

    Refactor UserCard component to enhance user details display
    - Updated the UserCard component to include a structured layout with user information such as name, email, and role, improving the visual presentation.
    - Added tooltips for joined date, enhancing user experience by providing additional context.
    - Implemented a responsive design using Card and CardContent components for better alignment and readability.

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -58,8 +58,60 @@ export const UserCard = ({
   loggedInUser, 
   onUserDeleteSuccess 
 }: Props) => {
+
+  const fetcher = useFetcher();
+
+
   return (
-    <div>UserCard</div>
+    <Card className="group py-4">
+      <CardContent className="grid grid-cols-[max-content_minmax(0,1fr)_max-content] gap-4 px-4">
+        <Avatar 
+          email={email}
+          size="40"
+          name={firstName || lastName ? [firstName, lastName].join(" ") : email}
+          className="rounded-lg"
+        />
+
+        <div>
+          {/* Badge "admin" */}
+          <div className="flex items-center gap-2">
+            <h3 className="font-semibold">
+              {firstName || lastName 
+                ? [firstName, lastName].join(" ") 
+                : username
+              }
+            </h3>
+
+            {role === "admin" && (
+              <Badge variant="outline" className="capitalize">
+                {role}
+              </Badge>
+            )}
+          </div>
+
+          {/* email */}
+          <p className="text-sm text-muted-foreground">
+            {email}
+          </p>
+
+          <div className="text-xs text-muted-foreground mt-2">
+            <Tooltip delayDuration={250}>
+              <TooltipTrigger>
+                Joined{" "}
+                {formatDistanceToNowStrict(createdAt, { addSuffix: true })}
+              </TooltipTrigger>
+
+              <TooltipContent side="right">
+                {new Date(createdAt).toLocaleString("en-US", {
+                  dateStyle: "medium",
+                  timeStyle: "short",
+                })}
+              </TooltipContent>
+            </Tooltip>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
   )
 }
 
