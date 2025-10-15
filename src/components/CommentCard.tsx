8d672e4dd6eb0de370a8f81a6a12adf851a334ce
Jorge Matos <jisapjisap@gmail.com>
2025-10-15 19:51:14 +0200

    Enhance CommentCard component with user details and interaction features
    - Updated the CommentCard component to include user email and improved avatar display.
    - Added conditional rendering for deleted accounts with tooltips for better user feedback.
    - Implemented a more structured layout for comment content, including likes and removal buttons, enhancing user interaction and visual clarity.

--- src/components/CommentCard.tsx
+++ src/components/CommentCard.tsx
@@ -24,7 +24,69 @@ export const CommentCard = ({ content, likesCount, user, blog, createdAt }: Prop
   return (
     <div className="@container">
       <div className="group flex flex-col items-start gap-4 p-4 rounded-xl hover:bg-accent/25 @md:flex-row">
-        <Avatar />
+        <Avatar 
+          email={user?.email} 
+          size="40"
+          round
+        />
+
+        <div className="flex flex-col gap-2 me-auto">
+          <div className="flex items-center gap-2">
+            {user ? (
+              <div className="text-sm text-muted-foreground">
+                @{user.username}
+              </div>
+            ):(
+              <div className="text-sm text-destructive/80 italic">
+                <Tooltip delayDuration={250}>
+                  <TooltipTrigger>
+                    Account deleted
+                  </TooltipTrigger>
+
+                  <TooltipContent>
+                    This account has been deleted.
+                  </TooltipContent>
+                </Tooltip>
+              </div>
+            )}
+
+            <div className="size-1 rounded-full bg-muted-foreground/50"></div>
+          
+            <div className="text-sm text-muted-foreground">
+              <Tooltip delayDuration={250}>
+                <TooltipTrigger>
+                  {formatDistanceToNow(createdAt, {addSuffix: true})}
+                </TooltipTrigger>
+
+                <TooltipContent>
+                  {new Date(createdAt).toLocaleString("en-US", {
+                    dateStyle: "long",
+                    timeStyle: "short",
+                  })}
+                </TooltipContent>
+              </Tooltip>
+            </div>
+          </div>
+
+          <div className="max-w-[60ch]">
+            {content}
+          </div>
+
+          <div className="flex items-center gap-2 mt-1">
+            <Button variant="ghost" aria-label="Like comment">
+              <ThumbsUpIcon />
+
+              {likesCount > 0 && (
+                <span className="sr-only">Total likes: ${likesCount}</span>
+              )}
+            </Button>
+
+            <Button variant="ghost" aria-label="Remove comment">
+              <TrashIcon />
+              Remove
+            </Button>
+          </div>
+        </div>
       </div>
     </div>
   )
